e526487277f41be955125f017cd7ca70c8e27d76
MrCrise <andrey121205@gmail.com>
2025-05-17 18:23:09 +0500

    Change db engine to postgresql

--- site/EduCounter/settings.py
+++ site/EduCounter/settings.py
@@ -75,10 +75,18 @@ WSGI_APPLICATION = 'EduCounter.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'educounter',
+        'USER': 'educounter_admin',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '',
+
     }
+
 }
 
 
